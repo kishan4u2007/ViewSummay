40b029d856075d70ba60abe48c2f92471728192e
Kishan Raikwar <kishan.raikwar@spectramd.com>
2024-04-15 15:14:26 +0530

    Add your commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import Summary from "./components/summary";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="main__wrap">
+        <Summary />
+      </div>
+    );
+  }
+}
+
+export default App;
