9b90cbd4c3c7e1063e09931b5e11c38471b8202d
Kishan Raikwar <kishan.raikwar@spectramd.com>
2024-04-15 15:18:58 +0530

    Updates

---
+++ precache-manifest.76e4dbd4f3e21c609a57ada94789e9ab.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb8f7290b186aa0d3261",
+    "url": "/react-gh-pages/static/css/main.7a3fb16e.chunk.css"
+  },
+  {
+    "revision": "bb8f7290b186aa0d3261",
+    "url": "/react-gh-pages/static/js/main.7966d510.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "2c02c442efc3e6d1d57b",
+    "url": "/react-gh-pages/static/css/2.ce996508.chunk.css"
+  },
+  {
+    "revision": "2c02c442efc3e6d1d57b",
+    "url": "/react-gh-pages/static/js/2.be2f4965.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-gh-pages/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-gh-pages/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-gh-pages/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-gh-pages/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-gh-pages/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "c51e2d218768cbaa1eacdb1c29115c24",
+    "url": "/react-gh-pages/index.html"
+  }
+];
